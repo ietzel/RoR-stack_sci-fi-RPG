dd35bba401908c776210852b483b359661179035
ietzel <67884277+ietzel@users.noreply.github.com>
2024-04-19 21:10:08 -0700

    Update Main.sql

--- Main.sql
+++ Main.sql
@@ -1,19 +1,19 @@
 CREATE DATABASE game;
 
 CREATE TABLE characters (
-  name TINYTEXT(128),
-  category TINYTEXT(64),
-  offense SMALLINT, --Athletics (climb, force open, grapple, high jump, long jump, reposition, shove, swim, trip; disarm),
-  defense SMALLINT, --n/a
-  speed SMALLINT, --Acrobatics (balance, tumble through; fly maneuver, squeeze), Piloting, Stealth (conceal object, hide, sneak), Thievery (palm object, steal; disable device, pick lock)
-  mental SMALLINT, --Arcana (recall knowledge; borrow an arcane spell, decipher writing, identify magic, learn a spell), Computers, Crafting (recall knowledge, repair; craft, earn income, identify alchemy), Deception (create diversion, impersonate, lie; feint), Diplomacy (gather information, make impression, request), Intimidate (coerce, demoralize), Lore (recall knowledge; earn income), Medicine (first aid; disease, poison, wounds), Nature (command animal, recall knowledge; identify magic, learn a spell), Occultism (recall knowledge; decipher writing, identify magic, learn a spell), Society (recall knowledge, subsist; create forgery, decipher writing), Survival (sense direction, subsist; cover tracks, track), Religion (recall knowledge; decipher writing, identify magic, learn a spell), Society (recall knowledge, subsist; create forgery), Survival (sense direction, subsist; cover tracks, track), Religion (recall knowledge; decipher writing, identify magic, learn a spell)
+  name TINYTEXT,
+  category TINYTEXT,
+  offense SMALLINT(2048), --Athletics (climb, force open, grapple, high jump, long jump, reposition, shove, swim, trip; disarm),
+  defense SMALLINT(2048), --n/a
+  speed SMALLINT(2048), --Acrobatics (balance, tumble through; fly maneuver, squeeze), Piloting, Stealth (conceal object, hide, sneak), Thievery (palm object, steal; disable device, pick lock)
+  mental SMALLINT(2048), --Arcana (recall knowledge; borrow an arcane spell, decipher writing, identify magic, learn a spell), Computers, Crafting (recall knowledge, repair; craft, earn income, identify alchemy), Deception (create diversion, impersonate, lie; feint), Diplomacy (gather information, make impression, request), Intimidate (coerce, demoralize), Lore (recall knowledge; earn income), Medicine (first aid; disease, poison, wounds), Nature (command animal, recall knowledge; identify magic, learn a spell), Occultism (recall knowledge; decipher writing, identify magic, learn a spell), Society (recall knowledge, subsist; create forgery, decipher writing), Survival (sense direction, subsist; cover tracks, track), Religion (recall knowledge; decipher writing, identify magic, learn a spell), Society (recall knowledge, subsist; create forgery), Survival (sense direction, subsist; cover tracks, track), Religion (recall knowledge; decipher writing, identify magic, learn a spell)
   crouching BOOLEAN,
-  x INT,
-  y INT,
-  z INT,
-  dX TINYINT(256),
-  dY TINYINT(256),
-  dZ TINYINT(256)
+  x INT(1000092),
+  y INT(1000092),
+  z INT(1000092),
+  dX TINYINT(128),
+  dY TINYINT(128),
+  dZ TINYINT(128)
 );
 
           /*this.solid = box(size/2, size/2, size*1.5); //grass or water
