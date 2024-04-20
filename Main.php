bf3c15e3e819961dbc91434fdeaff84e44497093
ietzel <67884277+ietzel@users.noreply.github.com>
2024-04-20 11:11:18 -0700

    Update Main.php

--- Main.php
+++ Main.php
@@ -3,12 +3,12 @@ $servername = "localhost";
 $username = "username";
 $password = "password";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=game", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
-echo "Connected successfully";
 ?>
