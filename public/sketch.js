09aa6a77b0e20dd31d09858a6214c7ca673467a4
staccblox <67884277+staccblox@users.noreply.github.com>
2024-06-16 18:46:35 -0700

    Add files via upload

--- public/sketch.js
+++ public/sketch.js
@@ -37,28 +37,28 @@ function damage(stat, mental, target, ranged) {
         }
     }
     if(!ranged) {
-        if(getDist(this.x, this.y, this.z, target.x, target.y, target.z) <= size*1.5) {
+        if(getDist(this.x, this.y, this.z, target.x, target.y, target.z) <= 8*1.5) {
             if((accuracystat-crouch_mod+frightened_mod+10) >= (getModOfStat(target.sped)-dexpenalty+t_crouch_mod)) {
                 if(((accuracystat*2)+frightened_mod) >= getModOfStat(target.dfse)) {
-                    characters = splice([indexOf(target), 1];
+                    characters = splice([indexOf(target), 1]);
                 }
             } else {
                 if(((accuracystat-cover_mod)+frightened_mod) >= (getModOfStat(target.sped)-dexpenalty+t_crouch_mod)) {
                     if((accuracystat+frightened_mod) >= getModOfStat(target.dfse)) {
-                        characters = splice([indexOf(target), 1];
+                        characters = splice([indexOf(target), 1]);
                     }
                 }
             }
         }
-    } else if(getDist(this.x, this.y, this.z, target.x, target.y, target.z) <= size*6) {
+    } else if(getDist(this.x, this.y, this.z, target.x, target.y, target.z) <= 8*6) {
         if((accuracystat-crouch_mod+frightened_mod+10) >= (getModOfStat(target.sped)-dexpenalty+(t_crouch_mod*-1))) {
             if(((accuracystat*2)+frightened_mod) >= getModOfStat(target.dfse)) {
-                characters = splice([indexOf(target), 1];
+                characters = splice([indexOf(target), 1]);
             }
         } else {
             if((accuracystat-+frightened_modcover_mod) >= (getModOfStat(target.sped)-dexpenalty+(t_crouch_mod*-1))) {
                 if((accuracystat+frightened_mod) >= getModOfStat(target.dfse)) {
-                    characters = splice([indexOf(target), 1];
+                    characters = splice([indexOf(target), 1]);
                 }
             }
         }
@@ -73,19 +73,21 @@ let water = 8;
 let x_coor = 0;
 let y_coor = -256;
 let z_coor = -512;
-let rez1, terrRange, seed;
+let rez1 = 0.01;
+let terrRange = 100; 
+let seed = millis()*10000;
 
 function keyPressed() {
     if (keyCode === LEFT_ARROW) {
-    x_coor -= 50;
+    	x_coor -= 50;
     } else if (keyCode === RIGHT_ARROW) {
-    x_coor += 50;
+    	x_coor += 50;
     } else if (keyCode === DOWN_ARROW) {
-    z_coor -= 50;
-    characters[0][0].crouching = true;
+    	z_coor -= 50;
+    	characters[0][0].crouching = true;
     } else if (keyCode === UP_ARROW) {
-    z_coor += 50;
-    characters[0][0].crouching = false;
+    	z_coor += 50;
+    	characters[0][0].crouching = false;
     }
 }
 
@@ -95,57 +97,57 @@ function setup() {
     cam = createCamera();
     //noLoop();
     colorMode(HSB,360,100,100,100);
-    rez1 = 0.01;
-    terrRange = 100;
-    seed = millis() * 10000; //to vary noise
+    //rez1 = 0.01;
+    //terrRange = 100;
+    //seed = millis() * 10000; //to vary noise
     draw();
 }
 
 function draw() {
-    cam.camera(x_coor,y_coor,z_coor,0,0,0,0,1,0);
+    cam.camera(0,-256,-512,0,0,0,0,1,0); //x_coor,y_coor,z_coor
 
     background(220,100,100);
-    for (x = -width/2-overlap; x < width/2+overlap; x+=size) {
-    for (z=-depth/2-overlap; z <depth/2+overlap; z+=size) {
-        y = (noise(x*rez1+seed, z*rez1+seed)-0.5)*terrRange*2;
+    for (x = -width/2-256; x < width/2+256; x+=8) {
+    for (z=-512/2-256; z <512/2+256; z+=8) {
+        y = (noise(x*0.01+(millis() * 10000), z*x*0.01+(millis() * 10000))-0.5)*100*2;
         stroke(0);
-        g = noise(x * rez1+seed+20000,z*rez1+seed+20000)-0.5;
+        g = noise(x * x*0.01+(millis() * 10000)+20000,z*x*0.01+(millis() * 10000)+20000)-0.5;
         fill(125+125*g,95+90*g,60+60*g,100) //grass color
-        if (y<treeLine) {
+        if (y<-64) {
         fill (200,20,40); //stone color
         }
-        if (y>water) {
+        if (y>8) {
         fill(240,100,60,100); //water color
-        y = water;
+        y = 8;
         noStroke();
         }
         push();
         translate(x,y,z);
-        box(size); //grass or water
-        translate(0,size*3,0);
+        box(8); //grass or water
+        translate(0,8*3,0);
         fill (200,20,40); //stone color
-        box(size,size*5,size); //stone block
-        let treeChance = (noise(x * rez1+seed+10000,z*rez1+seed+10000)-0.5)*90;
-        if (treeChance > 9.5 && y>treeLine && y<water) {
-        translate(0,-size*5,0);
+        box(8,8*5,8); //stone block
+        let treeChance = (noise(x * x*0.01+(millis() * 10000)+10000,z*x*0.01+(millis() * 10000)+10000)-0.5)*90;
+        if (((noise(x * x*0.01+(millis() * 10000)+10000,z*x*0.01+(millis() * 10000)+10000)-0.5)*90) > 9.5 && y>-64 && y<8) {
+        translate(0,-8*5,0);
         fill(0,75,30); //trunk color
-        cylinder(size/2,size*3);
-        translate(-size,-size,-size);
+        cylinder(8/2,8*3);
+        translate(-8,-8,-8);
         fill(120,80,30); //leaves
-        //sphere(size*2);
+        //sphere(8*2);
         // fill in tree:
         for (i=0;i<2;i++){
             for (j=0;j<3;j++){
             for (k=0;k<3;k++){
-                box(size);
-                translate(0,0,size)
+                box(8);
+                translate(0,0,8)
             }
-            translate(size,0,-size*3)
+            translate(8,0,-8*3)
             } 
-            translate(-size*3,-size,0)
+            translate(-8*3,-8,0)
         }
-        translate(size,0,size);
-        box(size) //cap on tree
+        translate(8,0,8);
+        box(8) //cap on tree
         }
         pop();
     }
