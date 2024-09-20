87baf5c214bc57409d1d14295b8d4079e8f772f2
staccblox <67884277+staccblox@users.noreply.github.com>
2024-09-20 17:28:20 -0400

    Update main.rb

--- main.rb
+++ main.rb
@@ -124,50 +124,7 @@ function damage(stat, mental, target, ranged): void {
 	    }
 	}
     }
-}
-
-const quotes = [
-	"You don't even know that you're alive, 81^3#!",
-	"Green number 4, close the door!",
-	"You're not perfect!",
-	"I-I-I-I-I want the knife.",
-	"Sharks and minnows!",
-	"You are not even past the phase yet.",
-	"Put your hand in the gom jabbar.",
-	"We're gonna drive that mother, down into your spine!",
-	"And the nazgul was no more.",
-	"At the end of a road was a white house.",
-	"Who shall you be, the payer, or the payee?",
-	"The game is predator vs prey.",
-	"Where is the scroll?",
-	"Just wait, until I've finished this section!",
-	"Of passion accurst I have drank of a water that quenches all thirst",
-	"Hell is empty and all the devils are here",
-	"Chainsaw sepukku!"
-	"It is a dump company.",
-	"Let's say we strike a deal, not reinvent the wheel!",
-	"We need to save the mentally ill.",
-	"It was bait.",
-	"Spinware is dark pattern.",
-	"You don't have to implement the program. You do have to implement the program.",
-	"Prometheus one.",
-	"Bio-sadist engine.",
-	"Engine of the uncontrolled.",
-	"Floating white gown.",
-	"Depressive marketing campaign.",
-	"Very repetitive excitement.",
-	"Round 2 electric boogaloo",
-	"It's bad for ya.",
-	"Sleep.",
-	"Intelligence is disease.",
-	"It shattered the kid's dreams.",
-	"They just keep driving, driving, driving.",
-	"Make sure it actually meets what they expect.",
-	"Don't be afraid of the dark.",
-	"Undead malware."
-	"Goatskull floating in green mist.",
-	"That was just my crazy ********: don't crap on me."
-];      
+}    
 
     <h1><a href="https://docs.google.com/document/d/1UnQN5FBOXWiEOR3lkoWNMFROX0AjwMuNREEij2Xrdvo/edit">Campaign</a> tips for character creation</h1>
     <form id="travel">Travel:
