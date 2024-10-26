1da28836eb753e96df0291e0a6ad86b38a275e5f
ietzel <67884277+ietzel@users.noreply.github.com>
2024-10-26 13:30:33 -0400

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,12 +8,12 @@ const summaries = [
   "Hell. Inner Sphere, Outer Sphere."
 ];
 var organisms = [
-  new organism("Player", 10, 10, 10, 10),
-  new organism("Werewolf", 20, 22, 20, 20),
-  new organism("Eremite Kyton", 30, 27, 30, 25),
-  new organism("Balor Demon", 35, 25, 36, 25),
-  new organism("Olethrodaemon", 35, 26, 37, 21),
-  new organism("Pit Fiend", 37, 30, 35, 27)
+  new organism("Player", 10, 10, 10, 10, 10, 10),
+  new organism("Werewolf", 20, 22, 20, 20, 20, 20),
+  new organism("Eremite Kyton", 30, 27, 30, 22, 21, 33),
+  new organism("Balor Demon", 35, 25, 36, 24, 24, 27),
+  new organism("Olethrodaemon", 35, 26, 37, 12, 26, 25),
+  new organism("Pit Fiend", 37, 30, 35, 26, 30, 26)
 ];
 const industries = [
   [new industry("Financial Intermediation", "", ""), new industry("Mining", "", ""), new industry("Community, Social and Personal Service Activities & Tourism", "", "")] 
@@ -23,10 +23,12 @@ const industries = [
 
 class organism {
     name: string;
-    offense: int;
-    agility: int;
-    defense: int;
-    mental: int;
+    strength: int;
+    dexterity: int;
+    constitution: int;
+    intelligence: int;
+    wisdom: int;
+    charisma: int;
   }
 }
 class industry {
@@ -39,7 +41,7 @@ function encounter(): void {
   alert("Encounter: ", summaries[document.getElementById("travel").selectedIndex], organisms[document.getElementById("travel").selectedIndex]);
   damage(organisms[0].offense, organisms[0].mental, organisms[document.getElementById("travel").selectedIndex], false);
 }
-function damage(stat, mental, target, ranged): void {
+function damage(stat, intelligence, wisdom, charisma, target, ranged): void {
     accuracystat = getModOfStat(stat);
     dexpenalty = 0;
     crouch_mod = 0;
@@ -47,11 +49,11 @@ function damage(stat, mental, target, ranged): void {
     t_cover_mod = 0;
     knowledgeable = false;
     frightened_mod = 0;
-    if(mental >= 30) {
+    if(intelligence >= 30) {
   knowledgeable = true;
     }
-    if(target.mental<=mental) {
-  frightened_mod = floor((target.mental-mental)/-10)-1;
+    if(target.wisdom<=charisma) {
+  frightened_mod = floor((target.wisdom-charisma)/-10)-1;
     }
     if(this.crouching <= -1) {
   crouch_mod = -2;
@@ -62,37 +64,37 @@ function damage(stat, mental, target, ranged): void {
   crouch_mod = -2;
   cover_mod = -2;
     }
-    if((this.sped+this.ment+frightened_mod)>=(this.ofns*2)) {
-  if((this.ment>target.ment) && (this.sped>target.ment)) {
-      dexpenalty = target.sped;
+    if((this.dexterity+this.wisdom+frightened_mod)>=(this.strength*2)) {
+  if((this.charisma>target.wisdom) && (this.dexterity>target.wisdom)) {
+      dexpenalty = target.dexterity;
   }
     } else {
-  if(this.ofns+frightened_mod>target.ment) {
-      dexpenalty = target.sped;
+  if(this.strength+frightened_mod>target.wisdom) {
+      dexpenalty = target.dexterity;
   }
     }
     if(!ranged) {
   if(getDist(this.x, this.y, this.z, target.x, target.y, target.z) <= 8*1.5) {
-      if((accuracystat-crouch_mod+frightened_mod+10) >= (getModOfStat(target.sped)-dexpenalty+t_crouch_mod)) {
-    if(((accuracystat*2)+frightened_mod) >= getModOfStat(target.dfse)) {
+      if((accuracystat-crouch_mod+frightened_mod+10) >= (getModOfStat(target.dexterity)-dexpenalty+t_crouch_mod)) {
+    if(((accuracystat*2)+frightened_mod) >= getModOfStat(target.constitution)) {
         characters = splice([indexOf(target), 1]);
     }
       } else {
-    if(((accuracystat-cover_mod)+frightened_mod) >= (getModOfStat(target.sped)-dexpenalty+t_crouch_mod)) {
-        if((accuracystat+frightened_mod) >= getModOfStat(target.dfse)) {
+    if(((accuracystat-cover_mod)+frightened_mod) >= (getModOfStat(target.dexterity)-dexpenalty+t_crouch_mod)) {
+        if((accuracystat+frightened_mod) >= getModOfStat(target.constitution)) {
       characters = splice([indexOf(target), 1]);
         }
     }
       }
   }
     } else if(getDist(this.x, this.y, this.z, target.x, target.y, target.z) <= 8*6) {
-  if((accuracystat-crouch_mod+frightened_mod+10) >= (getModOfStat(target.sped)-dexpenalty+(t_crouch_mod*-1))) {
-      if(((accuracystat*2)+frightened_mod) >= getModOfStat(target.dfse)) {
+  if((accuracystat-crouch_mod+frightened_mod+10) >= (getModOfStat(target.dexterity)-dexpenalty+(t_crouch_mod*-1))) {
+      if(((accuracystat*2)+frightened_mod) >= getModOfStat(target.constitution)) {
     characters = splice([indexOf(target), 1]);
       }
   } else {
-      if((accuracystat-+frightened_modcover_mod) >= (getModOfStat(target.sped)-dexpenalty+(t_crouch_mod*-1))) {
-    if((accuracystat+frightened_mod) >= getModOfStat(target.dfse)) {
+      if((accuracystat-+frightened_modcover_mod) >= (getModOfStat(target.dexterity)-dexpenalty+(t_crouch_mod*-1))) {
+    if((accuracystat+frightened_mod) >= getModOfStat(target.constitution)) {
         characters = splice([indexOf(target), 1]);
     }
       }
